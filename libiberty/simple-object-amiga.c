0188cc0abbf93c1fa36b5911b271faed305ec309
Stefan Reinauer <stefan.reinauer@coreboot.org>
2026-08-05 22:28:59 -0700

    libiberty: include stdint.h for Amiga object support
    Include stdint.h when configure found it before compiling the Amiga
    HUNK simple-object backend. Some Linux hosts do not expose the required
    integer definitions through the existing transitive includes.

--- libiberty/simple-object-amiga.c
+++ libiberty/simple-object-amiga.c
@@ -19,6 +19,10 @@ Foundation, 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
 #include "libiberty.h"
 #include "simple-object.h"
 
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#endif
+
 #ifdef HAVE_STDLIB_H
 #include <stdlib.h>
 #endif
