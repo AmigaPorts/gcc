26f6b86e61da6c3c1a10f4a95df8dcfc7d2779e8
Bernie Innocenti <bernie@codewiz.org>
2026-09-24 16:03:44 +0800

    testsuite: skip two more 128-byte alignment tests on m68k-amigaos
    alignas17.C and pr107058.c (which includes pr50459.c) request alignment 128, above the 8-byte hunk ceiling, and fail with excess errors; skip them like pr50459.c.
    
    https://github.com/AmigaPorts/m68k-amigaos-gcc/issues/57

--- gcc/testsuite/c-c++-common/pr107058.c
+++ gcc/testsuite/c-c++-common/pr107058.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target lto } */
+/* { dg-skip-if "small alignment" { m68k-*-amigaos* } } */
 /* { dg-options "-g -fdebug-types-section -flto -fno-checking" } */
 
 /* We should handle the C++ FE issue gracefully with -fno-checking.  */
